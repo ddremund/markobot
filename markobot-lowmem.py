3d786ce8af4d5f6496f63866fac4afc87799762b
Derek Remund <ddremund@gmail.com>
2016-02-23 15:23:55 -0600

    stubbing

---
+++ markobot-lowmem.py
@@ -0,0 +1,5 @@
+# markbot with minimum memory usage
+
+def create_chain(name, corpus, order):
+
+def update_chain(name, key, value):
--- markobot.py
+++ markobot.py
@@ -1,2 +1,9 @@
 # markobot
 
+def build_chain(corpus, order):
+
+def persist_chain(chain, name):
+
+def load_chain(name):
+
+def update_chain(chain, key, value):
